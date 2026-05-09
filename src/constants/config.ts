9c72fa96bc2620a816aac881aa23aa3673190f96
Pedro Palheta <palhetapedro11@gmail.com>
2026-05-09 20:55:14 -0300

    alterando instagram

--- src/constants/config.ts
+++ src/constants/config.ts
@@ -4,7 +4,7 @@ export const CONFIG = {
   whatsapp: "5562993180772",
   email: "levpsico@gmail.com",
   instagram: "@levpsico",
-  instagramUrl: "https://instagram.com/levpsico",
+  instagramUrl: "https://www.instagram.com/crer.ser_?igsh=ajVlYmZkd3R1bDBi",
   green: "#52B788",
   greenLight: "#D8F3DC",
   greenDark: "#2D6A4F",
